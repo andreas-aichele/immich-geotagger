a080baf4d2028d37c9b846484d6d9ec3d05e70aa
Andreas <120754898+andreas-aichele@users.noreply.github.com>
2026-09-21 10:22:03 +0200

    Set stable Android application ID for distribution

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         }
     }
 
-    namespace = "com.example.immich_geotagger"
+    namespace = "io.github.andreasaichele.immichgeotagger"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -35,7 +35,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.immich_geotagger"
+        applicationId = "io.github.andreasaichele.immichgeotagger"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
